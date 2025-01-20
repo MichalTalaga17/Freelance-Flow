a7dd93336decef9ca636c94aef2068780446a6f0
Michał Talaga <michal.talaga.programming@gmail.com>
2025-01-20 01:14:20 +0100

    Initial Commit

---
+++ FreelanceFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreelanceFlow
+//
+//  Created by Michał Talaga on 20/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreelanceFlow/FreelanceFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreelanceFlowApp.swift
+//  FreelanceFlow
+//
+//  Created by Michał Talaga on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FreelanceFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
